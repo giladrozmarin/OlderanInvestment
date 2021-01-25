142bfb110f25f1d0e0ffe0a65fdada9308a88ed2
giladrozmarin <19gilad@gmail.com>
2021-01-25 11:38:20 +0200

    add userlist

--- client/myapp/src/pages/imgView.js
+++ client/myapp/src/pages/imgView.js
@@ -1,27 +1,29 @@
- import {getImages} from '../utils/img'
- import React, { useState, useEffect } from 'react'
+import { getImages } from "../utils/img";
+import React, { useState, useEffect } from "react";
+import UserList from "../components/UserList";
 export default function ImgView() {
- 
+  const [img, setImg] = useState([]);
 
-    const [img,setImg] = useState([])
+  useEffect(() => {
+    getImages().then((res) => {
+      setImg(res);
+    });
+  }, []);
 
-    useEffect(() => {
-     getImages().then(res=> {
-         
-        setImg(res)
-    }
-     )},[])
-
-    return (
-        <>
-    <h1>10 random picture</h1>
-  <ul>
-      {
-        img.map(v =>{
-              return <img src={v.url} alt={v.url}/>
-            })
-      }
+  return (
+    <>
+      <h1>10 random picture</h1>
+      <ul>
+        {img.map((v) => {
+          return (
+            <img key={v.id} src={v.url} alt={v.url} width="300" height="200" />
+          );
+        })}
       </ul>
-</>
-    )
+  
+      {(sessionStorage["role"]) === "ADMIN" ?  <UserList/> : null}
+    </>
+
+  
+  );
 }
