f71c73995404f24d81bde90ab5b546b014361671
giladrozmarin <19gilad@gmail.com>
2021-01-25 11:51:31 +0200

    finish create user

--- client/myapp/src/components/CreateUser.js
+++ client/myapp/src/components/CreateUser.js
@@ -1,45 +1,128 @@
-
-import {useState} from 'react'
-import {createUser} from '../utils/users'
+import { useState } from "react";
+import { createUser } from "../utils/users";
+import {
+  Button,
+  TextField,
+  Typography,
+  Container,
+  CssBaseline,
+  Avatar,
+  MenuItem,
+  Select,
+  FormHelperText,
+  Link
+} from "@material-ui/core";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import { useStyles } from "../style/style";
+ 
 
 export default function CreateUser() {
+  const classes = useStyles();
  
-    const [user,setUser] = useState([])
-    const [message,setMessage] = useState('')
-    const handleChange = (e) => {
-        const {name,value} = e.target 
-        setUser( prevState => ({...prevState,[name]:value}))
-        
-    }
+  const [user, setUser] = useState([]);
+  const [role, setRole] = useState('');
+  const [err, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const handleRole = (event) => {
     
-    const handleSubmit = () => 
-    
-    createUser(user).then(res => {
-      res === true ? 
-      setMessage('user created successfully'):
-      res.Nickname === undefined?
-      setMessage(`This ${Object.keys(res)} :  ${res?.Username} is already exists`):
-      setMessage(`This ${Object.keys(res)} : ${res?.Nickname} is already exists`)
-    }) 
-    return (
+    setRole(event.target.value);
+  };
+  const handleSubmit = (e) => {
+   e.preventDefault();
+   user.role = role
+   let username = user.username
+  createUser(user).then((res) => {
+    res === true
+      ? setMessage(`${username} user created successfully`)
+      : res.Nickname !== undefined
+      ? setMessage(`This ${Object.keys(res)} :  ${res?.Nickname} is already exists`)
+      : res.Username !== undefined?
+      setMessage(`This ${Object.keys(res)} : ${res?.Username} is already exists`):
+      setMessage("Please fill all fields!!")
+  })}
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
 
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <PersonAddIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Create user
+        </Typography>
+        <form className={classes.form} validate>
+          <TextField
+            type="text"
+            name="nickname"
+            onChange={handleChange}
+            label="Enter your nickname"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            autoFocus
+            autoComplete="nickname"
+          />
+          <TextField
+            type="text"
+            name="username"
+            onChange={handleChange}
+            label="Enter your username"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            autoComplete="username"
+          />
+          <TextField
+            type="password"
+            name="password"
+            onChange={handleChange}
+            label="Enter your password"
+            variant="outlined"
+            margin="normal"
+            autoComplete="current-password"
+            required
+            fullWidth
+            
+          />
 
-       <>
-      
-       <input type="text" name="nickname" onChange={handleChange} placeholder="Enter your nickname"/>
-       <input type="text" name="username" onChange={handleChange}  placeholder="Enter your username"/>
-       <input type="password" name="password" onChange={handleChange} placeholder="Enter your password"/>
-       
+          <Select
+            htmlFor="roles"
+            name="roles"
+            id="roles"
+            value={role}
+            style={{ minWidth: 410 }}
+            onChange={handleRole}
+          >
 
-       <label htmlFor="roles">Choose a role:</label>
-       <select name="roles" id="roles" onChange={handleChange}  >
-       <option value="USER">user</option>
-       <option value="ADMIN">admin</option>
-       </select>
+            <MenuItem value="USER">user</MenuItem>
+            <MenuItem value="ADMIN">admin</MenuItem>
+          </Select>
+          <FormHelperText>Please chose user role</FormHelperText>
 
-       <input type="submit" value="Create User" onClick={handleSubmit}/>
-       <br/> 
-     {message}
-     </>
-    )
+          <Button
+            className={classes.submit}
+            type="submit"
+            value="Create User"
+            onClick={handleSubmit}
+            color="primary"
+            variant="contained"
+            fullWidth
+          >
+            Create User
+          </Button>
+        </form>
+      </div>
+      <Typography component="h6" variant="h6">{err}</Typography> 
+      <Link href="/" variant="body2">
+              {"Back to login"}
+            </Link>
+    </Container>
+  );
 }
