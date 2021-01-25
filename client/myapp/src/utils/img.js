e67a3a1e9623059a97b08c0f61b3ff18cff96014
giladrozmarin <19gilad@gmail.com>
2021-01-25 11:35:43 +0200

    .

--- client/myapp/src/utils/img.js
+++ client/myapp/src/utils/img.js
@@ -1,23 +1,18 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 const getAllImages = () => {
-
-    return  axios.get('https://jsonplaceholder.typicode.com/photos')
-    
-}
+  return axios.get("https://jsonplaceholder.typicode.com/photos");
+};
 
 const getImages = async () => {
-  
-   //get all images 
-   let data = await getAllImages()
-   //take 10 random element from data.data array 
-   let ten_img = Array.from({length:10},(v,k)=>k).map((value)=> {
+  //get all images
+  let data = await getAllImages();
+  //take 10 random element from data.data array
+  let ten_img = Array.from({ length: 10 }, (v, k) => k).map((value) => {
     //get num between 0 to 4999
     let random_num = Math.floor(Math.random() * 5000);
-    return value = data.data[random_num]
-   })
-   return ten_img; 
- 
-}
-export {getImages}
+    return (value = data.data[random_num]);
+  });
+  return ten_img;
+};
+export { getImages };
