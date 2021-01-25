49777a182dbdfefedfa2466aee837e01c834606b
giladrozmarin <19gilad@gmail.com>
2021-01-25 11:31:24 +0200

    .

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -1,6 +1,6 @@
  
 import './App.css';
-import { BrowserRouter as Router, Route, Switch,Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import CreateUser from './pages/createUser'
 import ImgView from './pages/imgView'
@@ -12,10 +12,7 @@ function App() {
     <>
   
     <Router>
-    <Link to={"/CreateUser"}> Create Account</Link> <br/>
-    <Link to={"/AllUsers"}>get All users</Link> <br/>
-    <Link to={"/ImgView"}> Image viewer</Link> <br/>
-    <Link to={"/"}> Login</Link> <br/>
+   
       <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/CreateUser" component={CreateUser} />
